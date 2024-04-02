8a68333cf539bfe32f74ce8330c73ee6028fbc8e
SPOILDSUSHI <zbslc@hotmail.com>
2024-04-01 20:05:50 -0500

    add poetry items

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
 
+
+
+export POETRY_VIRTUALENVS_IN_PROJECT=true
+
+# add this to zshrc
+export PATH="$HOME/.local/bin:$PATH"
+
+mkdir $ZSH_CUSTOM/plugins/poetry
+poetry completions zsh > $ZSH_CUSTOM/plugins/poetry/_poetry
