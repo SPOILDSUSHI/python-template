023a2eda80f4ebd0e22cb3130de05c5d30720259
SPOILDSUSHI <zbslc@hotmail.com>
2025-06-13 15:31:52 -0500

    update bootstrap and zshrc

--- .zshrc
+++ .zshrc
@@ -102,4 +102,6 @@ source $ZSH/oh-my-zsh.sh
 # Example aliases
 # alias zshconfig="mate ~/.zshrc"
 # alias ohmyzsh="mate ~/.oh-my-zsh"
+
+source "$HOME/.local/bin/env"
 eval "$(uv generate-shell-completion zsh)"
--- bootstrap_setup.sh
+++ bootstrap_setup.sh
@@ -6,11 +6,10 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 
 curl -LsSf https://astral.sh/uv/install.sh | sh
 
-cp -f .zshrc ~/.zshrc
-
-source $HOME/.local/bin/env
+dirpath=$(realpath "$0" | sed 's|\(.*\)/.*|\1|')
+abspath="${dirpath}/.zshrc"
+cp -f $abspath ~/.zshrc
 
 uv python install 3.12
 
 uv tool install pre-commit
-
